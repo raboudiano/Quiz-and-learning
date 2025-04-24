df4cf05bad8984533c2e63993cb75e13768a25aa
raboudiano <ibrahimraboudi09@gmail.com>
2025-04-24 19:48:10 +0100

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>📘 Personalized Learning App</h1>
+      <p>Let's build your custom quiz journey!</p>
+    </div>
+  );
+}
+export default App;
